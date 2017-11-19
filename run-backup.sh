0fa3c906e2267cff104b0051003bc7e6a217e1e5
phpfs <phpfs@users.noreply.github.com>
2017-11-19 16:04:14 +0100

    Fixed typo

--- run-backup.sh
+++ run-backup.sh
@@ -5,7 +5,7 @@ DATE=`date '+%Y-%m-%d_%H:%M:%S'`
 echo "--> Running Backup..."
 
 mysqldump --default-character-set=utf8mb4 -u${DBUSER} -p${DBPASS} -h mysql ${DBNAME} > /mysqldump.sql
-tar cvfz /backup/backup_vmail_${DATE}.tar.gz /vmail /mysqldump.sql
+tar cvfz /backup/backup_mailcow_${DATE}.tar.gz /vmail /mysqldump.sql
 s3 put /backup/ ${AWS_BUCKET}
 rm -rf /backup/*
 rm -rf /mysqldump.sql
