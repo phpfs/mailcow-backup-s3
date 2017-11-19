1f49a38b7f0988e1671c43fc96fba78ad02bade1
phpfs <phpfs@users.noreply.github.com>
2017-11-19 13:29:58 +0100

    Update run-backup.sh

--- run-backup.sh
+++ run-backup.sh
@@ -1,7 +1,13 @@
+#!/bin/sh
+
 DATE=`date '+%Y-%m-%d_%H:%M:%S'`
+
+echo "--> Running Backup..."
 
 mysqldump --default-character-set=utf8mb4 -u${DBUSER} -p${DBPASS} -h mysql ${DBNAME} > /mysqldump.sql
 tar cvfz /backup/backup_vmail_${DATE}.tar.gz /vmail /mysqldump.sql
 s3 put /backup/ ${AWS_BUCKET}
 rm -rf /backup/*
 rm -rf /mysqldump.sql
+
+echo "--> Backup finished!"
