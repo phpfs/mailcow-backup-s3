e3f9e6e00c61fce4112082bc20b3baeb388cc1cf
phpfs <phpfs@users.noreply.github.com>
2017-11-19 15:22:22 +0100

    Added Timezone

--- entry-point.sh
+++ entry-point.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-echo "Creating crontab..."
+echo "--> Fixing timezone..."
+cp /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+echo $TIMEZONE > /etc/timezone
+date
+echo "--> Creating crontab..."
 echo -e "$CRON_SCHEDULE ash /run-backup.sh\n" > /etc/crontabs/root
-echo "Starting cron daemon..."
+echo "--> Starting cron daemon..."
 crond -f
